027a88d2c1c0b9daf9dc10b0e2684479c459f46e
nathanchapman <nathanchapman@utexas.edu>
2016-08-27 22:40:50 -0500

    Add sinon for test spies

--- test/test.js
+++ test/test.js
@@ -1,8 +1,16 @@
-import { assert, expect } from 'chai'
+import sinon from 'sinon';
+import { assert, expect } from 'chai';
 import Main from '../src/Main';
 
 describe('TestSuite', () => {
   it('someFunction() should return `Wooo!`', () => {
     expect(new Main().someFunction()).to.equal('Wooo!');
+  });
+
+  it('someFunction() is called twice', () => {
+    const wrapper = new Main();
+    const spy = sinon.spy(wrapper, "someFunction");
+    spy();
+    expect(spy.callCount).to.equal(1);
   });
 });
