45eec0ee19c2151775de1f20930a7bdd1e391e1d
nathanchapman <nathanchapman@utexas.edu>
2016-08-31 17:09:54 -0500

    General code cleaup

--- preinstall.js
+++ preinstall.js
@@ -10,13 +10,13 @@
 
 var fs = require('fs');
 
-fs.stat('.npmignore', function(err, stat) {
+fs.stat('.npmignore', function (err, stat) {
   if (err && err.code == 'ENOENT') {
     fs.writeFile('.npmignore', 'coverage\nsrc\ntest\n.babelrc\n.codeclimate.yml\n.DS_Store\n.eslintignore\n.eslintrc\n.gitignore\n.istanbul.yml\n.jshintrc\n.travis.yml\n');
   }
 });
 
-fs.stat('.gitignore', function(err, stat) {
+fs.stat('.gitignore', function (err, stat) {
   if (err && err.code == 'ENOENT') {
     fs.writeFile('.gitignore', '# npm\nnode_modules\nnpm-debug.log\n\n# dotfiles\n.DS_Store\n\n# generated files\ncoverage\nlib\n');
   }
