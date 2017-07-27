71d4d836ad80cab33b03ccaeb2c28630568a0c99
nathanchapman <nathanchapman@utexas.edu>
2017-07-26 22:28:24 -0500

    Update npm publishing

--- preinstall.js
+++ preinstall.js
@@ -8,26 +8,43 @@
  * Licensed under the MIT license.
  */
 
-var fs = require('fs');
+const fs = require('fs');
 
-fs.stat('.npmignore', function (err, stat) {
-  if (err && err.code == 'ENOENT') {
-    fs.writeFile('.npmignore', 'coverage\nsrc\ntest\n.babelrc\n.codeclimate.yml\n.DS_Store\n.eslintignore\n.eslintrc\n.gitignore\n.istanbul.yml\n.jshintrc\n.travis.yml\n');
-  }
-});
+let npmignore = [
+    'coverage',
+    'src',
+    'test',
+    '.babelrc',
+    '.codeclimate.yml',
+    '.eslintignore',
+    '.eslintrc',
+    '.gitignore',
+    '.istanbul.yml',
+    '.jshintrc',
+    '.travis.yml',
+];
 
-fs.stat('.gitignore', function (err, stat) {
-  if (err && err.code == 'ENOENT') {
-    fs.writeFile('.gitignore', '# npm\nnode_modules\nnpm-debug.log\n\n# dotfiles\n.DS_Store\n\n# generated files\ncoverage\nlib\n');
-  }
-});
+fs.writeFileSync('.npmignore', npmignore.join('\n'));
 
-fs.readFile('package.json', 'utf8', function (err, data) {
-  if (err) return console.log(err);
-  var result = data.replace(/,\s*"preinstall": "node preinstall.js"/g, '');
-  fs.writeFile('package.json', result, 'utf8', function (err) {
-     if (err) return console.log(err);
-  });
-});
+let gitignore = [
+    '# dotfiles',
+    '.DS_Store',
+    '# generated files',
+    'coverage',
+    'lib',
+    '*.log',
+    'node_modules',
+    'package-lock.json'
+];
 
-fs.unlinkSync('preinstall.js');
+fs.writeFileSync('.gitignore', gitignore.join('\n'));
+
+// fs.readFile('package.json', 'utf8', function (err, data) {
+//   if (err) return console.log(err);
+//   var result = data.replace(/,\s*"preinstall": "node preinstall.js"/g, '');
+//   fs.writeFile('package.json', result, 'utf8', function (err) {
+//      if (err) return console.log(err);
+//   });
+// });
+//
+// fs.unlinkSync('preinstall.js');
---
+++ prepublish.js
@@ -0,0 +1,14 @@
+/*!
+ * prepublish.js (https://github.com/nathanchapman/babel-starter-kit)
+ *
+ * This file is used to set up the npm package for publishing.
+ * It empties the contents of the .npmignore file.
+ *
+ * Copyright (c) 2016 Nathan Chapman.
+ * Licensed under the MIT license.
+ */
+
+const fs = require('fs');
+
+fs.writeFileSync('.npmignore', '');
+// fs.unlinkSync('prepublish.js');
